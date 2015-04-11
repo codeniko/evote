d6369647a937cc4b85b23e6adb159038ad8469ae
Nikolay Feldman <nf139@cs.rutgers.edu>
2015-04-11 17:12:11 -0400

    try

--- client.js
+++ client.js
@@ -4,27 +4,24 @@ var tls = require('tls');
 var fs = require('fs');
 
 var options = {
+	// These are necessary only if using the client certificate authentication
 	key: fs.readFileSync('keys/ctf-key.pem'),
-	cert: fs.readFileSync('keys/ctf-cert.pem')
+	cert: fs.readFileSync('keys/ctf-cert.pem'),
+
+	// This is necessary only if the server uses the self-signed certificate
+	ca: [ fs.readFileSync('keys/cla-cert.pem') ]
 };
 
-try {
-var conn = tls.connect(8000, options, function() {
-	if (conn.authorized) {
-		console.log("Connection authorized by a Certificate Authority.");
-	} else {
-		console.log("Connection not authorized: " + conn.authorizationError)
-	}
-	conn.write("test");
-	console.log();
+var socket = tls.connect(8000, options, function() {
+	console.log('client connected',
+			socket.authorized ? 'authorized' : 'unauthorized');
+	process.stdin.pipe(socket);
+	process.stdin.resume();
 });
-
-} catch(e) {
-	console.log(e);
-}
-
-
-conn.on("data", function (data) {
-	console.log(data.toString());
-	conn.end();
+socket.setEncoding('utf8');
+socket.on('data', function(data) {
+	console.log(data);
+});
+socket.on('end', function() {
+	server.close();
 });
