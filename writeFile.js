1de892a0da99e9bc6bf9328f898d6990ec9436df
Joyce Wang <joycewang2002@yahoo.com>
2015-04-13 16:40:07 -0400

    users done

--- writeFile.js
+++ writeFile.js
@@ -5,16 +5,17 @@ var faker = require('./faker.js');
 var ssn = require('ssn');
 
 var ssnArray = [];
-for (var i = 0; i < 1000000; i++) {
+for (var i = 0; i < 10000; i++) {
   var uniqueSSN = ssn.generate();
   if (ssnArray.indexOf(uniqueSSN) == -1) {
     ssnArray.push(uniqueSSN);
     console.log(i + " " + uniqueSSN);
     var randomName = faker.name.findName();
-    var insertData = randomName + "|" + uniqueSSN;
+    var insertData = randomName + "|" + uniqueSSN + "\n";
     console.log(insertData);
     fs.appendFile('data/users.db', insertData, function(err) {
-      console.log(err);
     });
   }
 }
+
+
